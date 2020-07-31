29689affafb8793fcbffbe776f26aa43a5360602
minaNew10 <minadevdream@gmail.com>
2020-07-31 08:24:35 +0200

    feat: handle registeration and login errors

--- app/src/main/java/com/example/lawquiz/LoginFragment.kt
+++ app/src/main/java/com/example/lawquiz/LoginFragment.kt
@@ -1,8 +1,6 @@
 package com.example.lawquiz
 
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,8 +10,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.lawquiz.databinding.FragmentLoginBinding
-import com.google.firebase.auth.FirebaseAuthException
-import com.google.firebase.auth.FirebaseAuthWeakPasswordException
+import com.google.firebase.auth.*
 
 
 private const val TAG = "LoginFragment"
@@ -23,12 +20,9 @@ private const val TAG = "LoginFragment"
  * create an instance of this fragment.
  */
 class LoginFragment : Fragment() {
+    lateinit var loginToast: Toast
     private lateinit var loginViewModel: LoginViewModel
     private lateinit var binding : FragmentLoginBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -57,6 +51,10 @@ class LoginFragment : Fragment() {
         return binding.root
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        loginToast = Toast.makeText(activity,getString(R.string.msg_login_success),Toast.LENGTH_LONG)
+    }
     private fun setupViewModel() {
         loginViewModel.loggedInUser.observe(viewLifecycleOwner, Observer {
             it?.let {
@@ -64,18 +62,28 @@ class LoginFragment : Fragment() {
             } ?: Toast.makeText(activity,"session ended",Toast.LENGTH_LONG).show()
         })
         loginViewModel.authResult.observe(viewLifecycleOwner, Observer {
-
+            var toastMsg: String = ""
             if(it != null && it.isComplete){
 
-                if(it.isSuccessful){
-                    Toast.makeText(activity,"دخول ناجح",Toast.LENGTH_LONG).show()
+                if(!it.isSuccessful){
+                    toastMsg =
+                        when(it.exception){
+                            is FirebaseAuthInvalidUserException -> getString(R.string.err_msg_not_registered)
+                            is FirebaseAuthWeakPasswordException -> getString(R.string.err_msg_weak_psswrd)
+                            is FirebaseAuthEmailException -> getString(R.string.err_msg_email)
+                            is FirebaseAuthUserCollisionException -> getString(R.string.err_msg_user_data_is_registered)
+                            else -> it.exception.toString()
+                        }
+
                 }else{
-                    
-                    Toast.makeText(activity,it.exception?.message ?: "",Toast.LENGTH_LONG).show()
+                    toastMsg = getString(R.string.msg_login_success)
                 }
+
             }else{
-                Log.i(TAG, "setupViewModel: not coml" )
+                toastMsg = getString(R.string.err_msg_problem_in_psswrd_or_email)
             }
+            loginToast.setText(toastMsg)
+            loginToast.show()
         })
     }
 
@@ -87,6 +95,8 @@ class LoginFragment : Fragment() {
     }
     private fun signOut(){
         loginViewModel.signOut()
+        loginToast.setText(R.string.sign_out_msg)
+        loginToast.show();
     }
 
 }
