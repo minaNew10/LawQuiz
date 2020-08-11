a9f00c99b980b638b77baa16e784ba66a5428534
minaNew10 <minadevdream@gmail.com>
2020-08-11 05:53:46 +0200

    refractor: move view holder related code to viewHolder class

--- app/src/main/java/com/example/lawquiz/ui/categories/CategoriesAdapter.kt
+++ app/src/main/java/com/example/lawquiz/ui/categories/CategoriesAdapter.kt
@@ -5,25 +5,46 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lawquiz.R
-import com.example.lawquiz.utils.CategoryItemViewHolder
 
-class CategoriesAdapter : RecyclerView.Adapter<CategoryItemViewHolder>() {
+class CategoriesAdapter : RecyclerView.Adapter<CategoriesAdapter.CategoryItemViewHolder>() {
     var data = listOf<String>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryItemViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.item_categories,parent,false) as TextView
-        return CategoryItemViewHolder(view)
+        return CategoryItemViewHolder.from(parent)
     }
 
     override fun getItemCount() = data.size
 
     override fun onBindViewHolder(holder: CategoryItemViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item
+        holder.bind(item)
     }
+
+
+
+    /**
+     * ViewHolder that holds a single [TextView].
+     *
+     * A ViewHolder holds a view for the [RecyclerView] as well as providing additional information
+     * to the RecyclerView such as where on the screen it was last drawn during scrolling.
+     */
+    class CategoryItemViewHolder private constructor(val textView: TextView): RecyclerView.ViewHolder(textView){
+        companion object {
+            fun from(parent: ViewGroup): CategoryItemViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.item_categories, parent, false) as TextView
+                return CategoryItemViewHolder(view)
+            }
+        }
+        fun bind(
+            item: String
+        ) {
+            textView.text = item
+        }
+    }
+
 
 }
