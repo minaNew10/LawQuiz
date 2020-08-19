e1ad1a62cef9fc96b7057ef22d30206045a5f129
minaNew10 <minadevdream@gmail.com>
2020-08-19 15:13:37 +0200

    create viewModel factory for question fragment view Model

--- app/src/main/java/com/example/lawquiz/ui/questions/QuestionFragment.kt
+++ app/src/main/java/com/example/lawquiz/ui/questions/QuestionFragment.kt
@@ -1,6 +1,6 @@
 package com.example.lawquiz.ui.questions
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,27 +8,31 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.lawquiz.R
-import com.example.lawquiz.databinding.MainCategoriesFragmentBinding
 import com.example.lawquiz.databinding.QuestionFragmentBinding
 
-class QuestionFragmet : Fragment() {
+
+class QuestionFragment : Fragment() {
     private lateinit var questionClass : String
     private var _binding: QuestionFragmentBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var viewModel: QuestionFragmetViewModel
-
+    private lateinit var viewModelFactory: QuestionViewModelFactory
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = DataBindingUtil.inflate(inflater,
                     R.layout.question_fragment,container,false)
-        viewModel= ViewModelProvider(this).get(QuestionFragmetViewModel::class.java)
-        questionClass = QuestionFragmetArgs.fromBundle(requireArguments()).questionClassification
-        Toast.makeText(activity,questionClass,Toast.LENGTH_LONG).show()
+        questionClass = QuestionFragmentArgs.fromBundle(requireArguments()).questionClassification
+        viewModelFactory = QuestionViewModelFactory(questionClass)
+        viewModel= ViewModelProvider(this,viewModelFactory).get(QuestionFragmetViewModel::class.java)
+        viewModel.currCategory.observe(viewLifecycleOwner, Observer {
+             Toast.makeText(activity,it,Toast.LENGTH_LONG).show()
+        })
         return binding.root
     }
 
--- app/src/main/java/com/example/lawquiz/ui/questions/QuestionFragmetViewModel.kt
+++ app/src/main/java/com/example/lawquiz/ui/questions/QuestionFragmetViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.lawquiz.ui.questions
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.lawquiz.model.Category
 
-class QuestionFragmetViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+private const val TAG = "QuestionViewModel"
+class QuestionFragmetViewModel(questionClass: String) : ViewModel() {
+    private val _currCategory = MutableLiveData<String>()
+    val currCategory: LiveData<String>
+        get() = _currCategory
+    init {
+        _currCategory.value = questionClass
+    }
 }
